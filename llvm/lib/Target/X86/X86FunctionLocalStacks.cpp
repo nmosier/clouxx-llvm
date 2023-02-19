b3403fc5663e4e3e3673289f9d8a593000bc0414
Nicholas Mosier <nmosier@stanford.edu>
2023-02-19 16:18:09 +0000

    update

--- llvm/lib/Target/X86/X86FunctionLocalStacks.cpp
+++ llvm/lib/Target/X86/X86FunctionLocalStacks.cpp
@@ -6,8 +6,13 @@
 #include "llvm/IR/Module.h"
 #include "llvm/CodeGen/LiveIntervals.h"
 #include "llvm/Clou/Clou.h"
+#include "llvm/ADT/Statistic.h"
 
 using namespace llvm;
+
+#define DEBUG_TYPE "x86-fps"
+STATISTIC(NumMitigations, "Number of mitigations");
+STATISTIC(NumInstructions, "Number of instructions inserted");
 
 namespace {
 
@@ -30,7 +35,11 @@ namespace {
 	return false;
       }
       
-      const TargetInstrInfo *TII = MF.getSubtarget<X86Subtarget>().getInstrInfo();
+      const X86Subtarget& STI = MF.getSubtarget<X86Subtarget>();
+      const TargetInstrInfo *TII = STI.getInstrInfo();
+      const bool Is64Bit = STI.is64Bit();
+      const unsigned AddrBytes = Is64Bit ? 8 : 4;
+      const Align AddrAlign(AddrBytes);
       const Function& F = MF.getFunction();
       const GlobalValue *sp;
       if (clou::enabled.fps) {
@@ -45,35 +54,38 @@ namespace {
 	    const auto& DL = MBBI->getDebugLoc();
 
 	    if (clou::enabled.fps) {
-	      
 	      // store SP right before
-	      BuildMI(MBB, MBBI, DL, TII->get(X86::MOV64mr))
-		.addReg(X86::RIP)
+	      // if (!MF.getFunction().doesNotRecurse())
+	      BuildMI(MBB, MBBI, DL, TII->get(Is64Bit ? X86::MOV64mr : X86::MOV32mr))
+		.addReg(Is64Bit ? X86::RIP : X86::NoRegister)
 		.addImm(1)
 		.addReg(0)
 		.addGlobalAddress(sp, 0)
 		.addReg(0)
+		.addReg(Is64Bit ? X86::RSP : X86::ESP)
 		.addMemOperand(MBB.getParent()->getMachineMemOperand(MachinePointerInfo(sp),
 								     MachineMemOperand::MOStore,
-								     8,
-								     Align(8)))
-		.addReg(X86::RSP);
-
+								     AddrBytes,
+								     AddrAlign));
+	      ++NumInstructions;
 	    }
 
-	    if (clou::enabled.prech) {
+	    if (clou::enabled.prech && Is64Bit) {
 	    
 	      // Zero out any unused parameter-passing registers
-	      for (const auto param_gpr : std::array<MCPhysReg, 6> {X86::EDI, X86::ESI, X86::EDX, X86::ECX, X86::R8D, X86::R9D}) {
+	      for (const auto param_gpr : std::array<MCPhysReg, 6> {X86::EDI, X86::ESI, X86::EDX,
+								    X86::ECX, X86::R8D, X86::R9D}) {
 		if (!callUsesRegister(*MBBI, param_gpr, TRI)) {
 		  // zero it out
 		  BuildMI(MBB, MBBI, DL, TII->get(X86::XOR32rr), param_gpr)
-		    .addReg(param_gpr)
-		    .addReg(param_gpr);
+		  .addReg(param_gpr)
+		  .addReg(param_gpr);
+		  ++NumInstructions;
 		}
 	      }
 
-	      std::array<MCPhysReg, 8> param_fps = {X86::XMM0, X86::XMM1, X86::XMM2, X86::XMM3, X86::XMM4, X86::XMM5, X86::XMM6, X86::XMM7};
+	      std::array<MCPhysReg, 8> param_fps = {X86::XMM0, X86::XMM1, X86::XMM2, X86::XMM3,
+						    X86::XMM4, X86::XMM5, X86::XMM6, X86::XMM7};
 	      if (any_of(param_fps, [&] (const MCPhysReg& reg) {
 		return callUsesRegister(*MBBI, reg, TRI);
 	      })) {
@@ -82,11 +94,20 @@ namespace {
 		    BuildMI(MBB, MBBI, DL, TII->get(X86::XORPSrr), reg)
 		      .addReg(reg)
 		      .addReg(reg);
+		    ++NumInstructions;
 		  }
 		}
 	      } else {
-		BuildMI(MBB, MBBI, DL, TII->get(X86::VZEROALL));	      
+#if 0
+		BuildMI(MBB, MBBI, DL, TII->get(X86::VZEROALL));
+#endif
+		++NumInstructions;
 	      }
+
+	      ++NumMitigations;
+
+	      if (clou::InsertTrapAfterMitigations)
+		BuildMI(MBB, MBBI, DL, TII->get(X86::MFENCE));
 
 	    }
 
@@ -100,35 +121,46 @@ namespace {
 		// cmovne rsp, rdi
 		// cmovne rbp, rdi
 		// cmovne rbx, rdi
-		BuildMI(MBB, MBBI_next, DL, TII->get(X86::XOR32rr), X86::EDI)
-		  .addReg(X86::EDI)
-		  .addReg(X86::EDI);
-#if 1
-		BuildMI(MBB, MBBI_next, DL, TII->get(X86::CMP64rm))
-		  .addReg(X86::RSP)
-		  .addReg(X86::RIP)
+		const MCRegister ZeroReg = Is64Bit ? X86::RDI : X86::EDX;
+		const MCRegister ZeroReg32 = Is64Bit ? X86::EDI : X86::EDX;
+		BuildMI(MBB, MBBI_next, DL, TII->get(X86::XOR32rr), ZeroReg32)
+		  .addReg(ZeroReg32)
+		  .addReg(ZeroReg32);
+		++NumInstructions;
+		
+		BuildMI(MBB, MBBI_next, DL, TII->get(Is64Bit ? X86::CMP64rm : X86::CMP32rm))
+		  .addReg(Is64Bit ? X86::RSP : X86::ESP)
+		  .addReg(Is64Bit ? X86::RIP : X86::NoRegister)
 		  .addImm(1)
 		  .addReg(0)
 		  .addGlobalAddress(sp, 0)
 		  .addReg(0)
 		  .addMemOperand(MBB.getParent()->getMachineMemOperand(MachinePointerInfo(sp),
 								       MachineMemOperand::MOStore,
-								       8,
-								       Align(8)));
-#endif
-#if 1
+								       AddrBytes,
+								       AddrAlign));
+		++NumInstructions;
+		
 		if (clou::enabled.fps) {
-		  for (const auto reg : {X86::RSP, X86::RBP, X86::RBX}) {
-		    BuildMI(MBB, MBBI_next, DL, TII->get(X86::CMOV64rr), reg)
+		  static const MCRegister CalleePreserved32[3] = {X86::ESP, X86::EBP, X86::EBX};
+		  static const MCRegister CalleePreserved64[3] = {X86::RSP, X86::RBP, X86::RBX};
+		  const MCRegister (*CalleePreserved)[3] = Is64Bit ? &CalleePreserved64 : &CalleePreserved32;
+		  for (const auto reg : *CalleePreserved) {
+		    BuildMI(MBB, MBBI_next, DL, TII->get(Is64Bit ? X86::CMOV64rr : X86::CMOV32rr), reg)
 		      .addReg(reg)
-		      .addReg(X86::RDI)
+		      .addReg(ZeroReg)
 		      .addImm(X86::COND_NE);
+		    ++NumInstructions;
 		  }
 		}
-#endif
+
+		++NumMitigations;
+
+		if (clou::InsertTrapAfterMitigations)
+		  BuildMI(MBB, MBBI_next, DL, TII->get(X86::MFENCE));
 	      
 	      }
-		
+	      
 	    }
 	  }
 	}
@@ -137,10 +169,28 @@ namespace {
       if (clou::enabled.fps) {
 	
 	// well, save the base pointer at least
-	static const MCPhysReg csrs[] = {X86::RBX};
+	const MCPhysReg csrs[] = {Is64Bit ? X86::RBX : X86::EBX};
 	MF.getRegInfo().setCalleeSavedRegs(csrs);
-
+	
       }
+
+#if 0
+      for (const MachineBasicBlock& MBB : MF) {
+	for (const MachineInstr& MI : MBB) {
+	  for (const MachineOperand& MO : MI.operands()) {
+	    if (MO.isReg()) {
+	      const Register Reg = MO.getReg();
+	      if (Reg.isPhysical()) {
+		const unsigned Bits = TRI.getRegSizeInBits(*TRI.getMinimalPhysRegClass(Reg.asMCReg()));
+		if (Bits == 64) {
+		  errs() << "64 BITS: " << MI;
+		}
+	      }
+	    }
+	  }
+	}
+      }
+#endif
       
       return true;
     }
